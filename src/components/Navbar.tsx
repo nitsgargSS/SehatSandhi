9536bd70988b8042550ebae8e09ac50b69ee9b13
Nitin Garg <nitingarg@Nitins-MacBook-Air.local>
2026-07-05 13:18:48 +0530

    Increase logo size in Navbar - 3

--- src/components/Navbar.tsx
+++ src/components/Navbar.tsx
@@ -14,7 +14,7 @@ export default function Navbar() {
         <div className="flex items-center justify-between h-16">
           {/* Logo */}
           <Link to="/" className="flex items-center">
-            <img src="/logo.png" alt="Sehatsandhi" className="h-22 w-auto" />
+            <img src="/logo.png" alt="Sehatsandhi" className="h-18 w-auto" />
           </Link>
 
           {/* Desktop nav */}
